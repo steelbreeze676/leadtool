c4ae6e739007f48f0b93912a9c97494abe7eba26
steelbreeze676 <michaelgovoni@hotmail.com>
2016-07-01 01:07:29 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+	return render_template('SalesTool.html')
+
+if __name__ == "__main__":
+		app.run()
+
+
